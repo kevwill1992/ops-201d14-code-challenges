89d28fa4d60b07a0a4d00819368162632e678040
kevwill1992 <kevinwilliams@fullsail.edu>
2023-10-24 23:46:48 -0700

    Second Attempt

---
+++ helloworld.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Hello World"
+# Script Name:      hello world            Script Name
+# Author:        kevin               Your Name
+# Date of latest revision:      10/24/2023
+# Purpose:                      Purpose
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
